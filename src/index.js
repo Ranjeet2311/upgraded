60496dcb3e1a33b1d4c99723c5ebc4baeae84061
Ranjeet kumar <ranjeetkumar2311@gmail.com>
2024-07-17 11:59:40 +0200

    Update React library and dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import "./style/style.scss"
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './style/style.scss';
 
+const root = createRoot(document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
