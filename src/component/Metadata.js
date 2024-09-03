fe3c1c257b056cd6a4898b55feed4368490ff00b
Ranjeet kumar <ranjeetkumar2311@gmail.com>
2024-09-03 15:24:15 +0200

    Update metadata image

--- src/component/Metadata.js
+++ src/component/Metadata.js
@@ -4,12 +4,9 @@ export default function Metadata({ title, description }) {
   return (
     <Helmet>
       <title> {title} </title>
+      <meta name="description" content={description} />
       <meta property="og:title" content="Ranjeet Kumar|Portfolio" />
       <meta property="og:description" content={description} />
-      <meta
-        property="og:image"
-        content="https://ranjeet2311.github.io/upgraded/images/social_card.png"
-      />
     </Helmet>
   );
 }
